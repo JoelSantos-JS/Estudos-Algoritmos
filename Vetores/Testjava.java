9147b620ac14ec8de5c70f2bbc677e8840c43380
Joel Santos <joeltere9@gmail.com>
2023-05-31 16:55:24 -0300

    day 31

---
+++ Vetores/Testjava.java
@@ -0,0 +1,16 @@
+package Vetores;
+
+import java.util.Stack;
+
+public class Testjava {
+
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        stack.push("1");
+        stack.push("2");
+        stack.push("3");
+
+        System.out.println(stack.pop());
+
+    }
+}
