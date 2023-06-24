01e3a53984faa83b21aaf0b9f68e91694ef0be47
Joel Santos <joeltere9@gmail.com>
2023-06-24 10:19:05 -0300

    day 24 method get and set

--- Program.java
+++ Program.java
@@ -3,12 +3,17 @@ import Pacote.PrintList;
 public class Program {
     public static void main(String[] args) {
 
-        PrintList printList = new PrintList(1);
+        PrintList printList = new PrintList(0);
 
+        printList.append(1);
         printList.append(2);
-        printList.printList();
-        printList.pretend(5);
+        printList.append(3);
 
+        printList.printList();
+
+        System.out.println(printList.get(2));
+
+        printList.set(3, 10);
         printList.printList();
 
     }
